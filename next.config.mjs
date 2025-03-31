29b5f9e2cd95531fed4871786bd2ed2148b10484
souldvn <emilevil97@gmail.com>
2025-03-31 13:35:37 +0700

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 export default nextConfig; 
